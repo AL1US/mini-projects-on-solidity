efb32ccb9ef56e1fa7079145e795ba781325de16
AL1US <Matvey.Ale.Devs@gmail.com>
2025-11-13 02:12:34 -0800

    Добавлена функции покупки токена из магазина юзеров

--- Alchemy.sol
+++ Alchemy.sol
@@ -26,12 +26,12 @@ contract Xcoin is ERC20, ERC1155 {
     struct structElement {
         string name;
         uint256 price; 
-        string rarity;
+        string rarity; // на самом деле редкость тут чисто по приколу
         uint256 amount;
     }
 
     struct structStoreUsers {
-        address ownerProduct;
+        address ownerToken;
         string name;
         uint256 price;
         string rarity;
@@ -56,10 +56,10 @@ contract Xcoin is ERC20, ERC1155 {
     function setElement(
         uint256 _index,
         uint256 amount) public payable {
+            require(_index <= 3, "Not found element"); //   Всего есть 4 базовых элемента от 0 до 3
+            uint256 price = 10;
             // Проверка на то есть ли уже этот токен у юзера, если есть, то нужно просто добавить к нему количество
-            require(balanceOf(msg.sender) >= 100 * amount, "you dont have enough money" );
-
-            uint256 price = 100;
+            require(balanceOf(msg.sender) >= price * amount, "you dont have enough money" );
 
             ERC1155._mint( 
                 msg.sender,
@@ -68,11 +68,11 @@ contract Xcoin is ERC20, ERC1155 {
                 ""
             );
 
-            transfer(address(this), 100);
+            transfer(address(this), price);
 
 
         // Если бы мы просто старались запушить по [_index], то у нас выдалобы ошибку, которая связана с тем, что
-        // мы по сути просто передадим массив строк, что передасться только в 1 поле структуры, а не по все другие, да и тип данныых 
+        // мы по сути просто передадим массив строк, что передасться только в 1 поле структуры, а не по все другие, да и тип данных 
         // там не правильный, поэтому оно впринципе нормально не передасться
         // Сохраняем мы это всё для удобства, чтобы потом можно было нормально и красиво отабразить, хотя не знаю пригодится ли оно мне
         userElements[msg.sender].push(
@@ -89,19 +89,21 @@ contract Xcoin is ERC20, ERC1155 {
 
     }
 
+    // Кликер для получения Xcoin
     function setToken() public payable {
 
         _transfer(address(this), msg.sender, 10);
     }
 
     function addTokenInStore(uint256 _price, uint256 _amount, uint256 _index) public {
-        // Проверка на то хватает ли токенов
+
+        require(balanceOf(msg.sender, _index) > 0, "Not found this token"); // Если токен не найден, то оно просто вернёт 0
 
         store.push(structStoreUsers(
             msg.sender,
-            arrayBaseElement[_index][0],
+            arrayBaseElement[_index][0], // имя
             _price,
-            arrayBaseElement[_index][2],
+            arrayBaseElement[_index][2], // редкость
             _amount
         ));
         
@@ -109,9 +111,26 @@ contract Xcoin is ERC20, ERC1155 {
         // Код, который удаляет токен если всё раскупили
     }
 
+    function buyTokenInStore(uint256 _index, uint256 _amount) public payable {
+        // Вытаскиваем значения, чтобы с ними можно было удобно работать
+        uint256 priceToken = store[_index].price * _amount; 
+        address addressOwnerToken = store[_index].ownerToken;
+        bytes memory data = "";
+
+        require(balanceOf(msg.sender) >= priceToken, "Not enough Xcoin"); 
+
+        transfer(addressOwnerToken, priceToken);
+
+        safeTransferFrom(addressOwnerToken, msg.sender, _index, _amount, data);
+    }
+
     // Геттеры
     function getAllElements() public view returns(string[] memory){
         return allBaseElement;
+    }
+
+    function getTokenInStore(uint256 _index) public view returns (structStoreUsers memory) {
+        return store[_index];
     }
     
 
