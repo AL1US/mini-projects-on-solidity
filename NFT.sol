f6f0af1d27b09e3250566262a0cf625b0ab7e5cd
AL1US <Matvey.Ale.Devs@gmail.com>
2025-11-23 22:05:34 -0800

    Сделана нормальная работа магазина (полностью)
    Осталось только допилить аукцион

--- NFT.sol
+++ NFT.sol
@@ -237,6 +237,11 @@ contract Xcoin is ERC20, ERC1155 {
     }
 
     // Работа с магазином
+    // По сути эта функция уже есть в контракте, но там нужно вводить адрес и всё такое, а тут сразу кнопка
+    function approveForContract() public {
+        setApprovalForAll(address(this), true);
+    }
+
     function setNFTInStore(
         uint256 _id,
         uint256 _amount,
@@ -244,6 +249,8 @@ contract Xcoin is ERC20, ERC1155 {
     ) public {
         bool found = false;
         uint256 foundIndex = 0;
+        bytes memory data = "";
+
 
         for (uint256 i = 0; i < userNFTs[msg.sender].length; i++) {
             if (userNFTs[msg.sender][i].id == _id) {
@@ -257,6 +264,8 @@ contract Xcoin is ERC20, ERC1155 {
 
         require(found, "NFT not found");
 
+        safeTransferFrom(msg.sender, address(this), _id, _amount, data);
+
         userNFTs[msg.sender][foundIndex].quanity -= _amount;
 
         storeNFT.push(
@@ -269,30 +278,44 @@ contract Xcoin is ERC20, ERC1155 {
         );
     }
 
+    function cancelNFTSale(uint256 _tokenId) public {
+        for (uint256 i = 0; i < storeNFT.length; i++) {
+            if (storeNFT[i].id == _tokenId && storeNFT[i].owner == msg.sender) {
+                
+                uint256 amount = storeNFT[i].amount;
+
+                // Возвращаем NFT
+                _safeTransferFrom(address(this), msg.sender, _tokenId, amount, "");
+
+                // Возвращаем количество NFT юзеру
+                for (uint256 j = 0; j < userNFTs[msg.sender].length; j++) {
+                    if (userNFTs[msg.sender][j].id == _tokenId) {
+                        userNFTs[msg.sender][j].quanity += amount;
+                        break;
+                    }
+                }
+
+                // Удаляем
+                storeNFT[i] = storeNFT[storeNFT.length - 1];
+                storeNFT.pop();
+                break;
+            }
+        }
+    }
+
     // Добавить коллекцию в магазин
     function setCollectionInStore(
         uint256 _id,
         uint256 _amount,
         uint256 _price
     ) public {
-
-        bool found = false;
-        uint256 foundIndex = 0;
-
-        for (uint256 i = 0; i < userCollectionsNFTs[msg.sender].length; i++) {
-            if (userCollectionsNFTs[msg.sender][i].id == _id) {
-                    found = true;
-                    foundIndex = i;
-                    break;
-            }
-        }
-
         require(collectionNFTs[_id].id == _id, "Collection not found");
-        require(owner_collection[_id] == msg.sender, "You are not owner this collection");
+        require(owner_collection[_id] == msg.sender, "You are not owner");
         require(_amount > 0, "Amount must be > 0");
-        require(!collectionNFTs[_id].state, "Collection already in store");
+        require(!collectionNFTs[_id].state, "Already in store");
 
-        userCollectionsNFTs[msg.sender][foundIndex].state = true;
+        collectionNFTs[_id].state = true;
+        collectionNFTs[_id].price = _price; 
 
         storeCollectionNFT.push(
             structNFTsInSomething(
@@ -302,6 +325,19 @@ contract Xcoin is ERC20, ERC1155 {
                 _price
             )
         );
+    }
+
+    function cancelCollectionSale(uint256 _collectionId) public {
+        for (uint256 i = 0; i < storeCollectionNFT.length; i++) {
+            if (storeCollectionNFT[i].id == _collectionId && storeCollectionNFT[i].owner == msg.sender) {
+                
+                collectionNFTs[_collectionId].state = false; // true -> in store / false -> not in store. Можно сделать так
+                                                             // потомучто у нас нет колличества на коллекциях                                            
+                storeCollectionNFT[i] = storeCollectionNFT[storeCollectionNFT.length - 1];
+                storeCollectionNFT.pop();
+                break;
+            }
+        }
     }
 
     // Покупка NFT
@@ -318,14 +354,68 @@ contract Xcoin is ERC20, ERC1155 {
 
         transfer(ownerToken, priceToken);
 
-        safeTransferFrom(ownerToken, msg.sender, storeNFT[_index].id, _amount, data);
+        safeTransferFrom(address(this), msg.sender, storeNFT[_index].id, _amount, data);
 
-        amountNFT -= _amount;
+        storeNFT[_index].amount -= _amount;
 
-        if (amountNFT == 0) {
-            storeNFT[_index] = storeNFT[storeNFT.length -1];
+        if (storeNFT[_index].amount == 0) {
+            storeNFT[_index] = storeNFT[storeNFT.length - 1];
             storeNFT.pop();
         }
+    }
+    // Покупка коллекции
+    function buyCollection(uint256 _collectionId) public {
+
+        uint256 storeIndex = 0;
+        for (uint256 i = 0; i < storeCollectionNFT.length; i++) {
+            if (storeCollectionNFT[i].id == _collectionId) {
+                storeIndex = i;
+                break;
+            }
+        }
+        require(storeIndex < storeCollectionNFT.length, "Collection not in store");
+
+        address seller = storeCollectionNFT[storeIndex].owner;
+        uint256 price = storeCollectionNFT[storeIndex].price;
+
+        require(seller != msg.sender, "Cannot buy own collection");
+        require(balanceOf(msg.sender) >= price, "Not enough Xcoin");
+
+
+        _transfer(msg.sender, seller, price);
+
+        structCollectionNFT storage col = collectionNFTs[_collectionId];
+
+        // хренатень чтобы передать все нфт из коллекции
+        for (uint256 j = 0; j < col.NFTInCollection.length; j++) {
+            _safeTransferFrom(
+                address(this),
+                msg.sender,
+                col.NFTInCollection[j].id,
+                col.NFTInCollection[j].amount,
+                ""
+            );
+        }
+
+        // Так как продавец больше не владелец, надо удалить его коллекцию
+        for (uint256 k = 0; k < userCollectionsNFTs[seller].length; k++) {
+            if (userCollectionsNFTs[seller][k].id == _collectionId) {
+                userCollectionsNFTs[seller][k] = userCollectionsNFTs[seller][userCollectionsNFTs[seller].length - 1];
+                userCollectionsNFTs[seller].pop();
+                break;
+            }
+        }
+        // пушим юзеру его только что купленную коллекцию
+        userCollectionsNFTs[msg.sender].push(col);
+
+        // Обновляем владельца
+        owner_collection[_collectionId] = msg.sender;
+        col.state = false;
+        col.price = 0;
+
+        // Удаляем из магазина
+        storeCollectionNFT[storeIndex] = storeCollectionNFT[storeCollectionNFT.length - 1];
+        storeCollectionNFT.pop();
     }
 
     constructor() ERC20("Xcoin", "X") ERC1155("./images/") {
@@ -343,7 +433,9 @@ contract Xcoin is ERC20, ERC1155 {
         // user[0x90F79bf6EB2c4f870365E785982E1f101E93b906] = structUser("Jack", "PROFI15d32024", 0);
         // ERC20._transfer(owner, 0x90F79bf6EB2c4f870365E785982E1f101E93b906, 400_000);
 
-        // remix
+        // remix    
+
+
         user[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = structUser(
             "Tom",
             "PROFI4B202024",
@@ -377,4 +469,4 @@ contract Xcoin is ERC20, ERC1155 {
             400_000
         );
     }
-}
+}   
